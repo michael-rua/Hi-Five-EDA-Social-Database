49dfbaae19c25210dd7e7f47d177a54d672e719a
Ben Mitchell <ben.mitchell11@hotmail.co.nz>
2020-08-21 11:20:02 +1200

    Went over our roles and got ready to push forward

--- migrations/20200820185136_users.js
+++ migrations/20200820185136_users.js
@@ -1,12 +1,12 @@
 exports.up = (knex) => {
-  return knex.schema.createTable('users', (table) => {
-    table.increments('id').primary()
-    table.string('name')
-    table.string('email')
-    table.integer('Phone')
-  })
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id').primary()
+        table.string('name')
+        table.string('email')
+        table.integer('phone')
+    })
 }
 
 exports.down = (knex) => {
-  return knex.schema.dropTable('users')
+    return knex.schema.dropTable('users')
 }
