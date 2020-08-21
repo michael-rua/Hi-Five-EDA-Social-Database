eaa1ffd65ebe45e3e392532275cb2b952c4997fa
michael-rua <michaelrua1@gmail.com>
2020-08-21 18:09:30 +1200

    prep for heroku deployment

--- seeds/1-users.js
+++ seeds/1-users.js
@@ -1,21 +1,21 @@
 exports.seed = (knex) => {
     return knex('users').insert([{
-        id: 1,
+        
         name: 'Michael Rua',
         email: 'michaelrua1@gmail.com',
         phone: '0211560222'
     }, {
-        id: 2,
+        
         name: 'Gideon Cooper',
         email: 'Gideon.cooper@hotmail.com',
         phone: '02108586647'
     }, {
-        id: 3,
+        
         name: 'Tatiana Bernon',
         email: 'tatiana.bernon@gmail.com',
         phone: '0273832501'
     }, {
-        id: 4,
+        
         name: 'Ben Mitchell',
         email: 'ben.mitchell11@hotmail.co.nz',
         phone: '0211497695'
